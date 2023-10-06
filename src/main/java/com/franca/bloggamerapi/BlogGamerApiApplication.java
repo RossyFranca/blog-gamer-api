ec304fed2ea8ce05bf2eaaa3308eb372e1d26875
RossyFranca <rossyfranca@gmail.com>
2023-10-06 01:11:11 -0300

    Initial commit

---
+++ src/main/java/com/franca/bloggamerapi/BlogGamerApiApplication.java
@@ -0,0 +1,13 @@
+package com.franca.bloggamerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogGamerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogGamerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franca/bloggamerapi/BlogGamerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franca.bloggamerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogGamerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
